abc6fee6e9884c72ca92f5be94132ee3d8f9b30b
leejlaa <lejladoric00@gmail.com>
2023-09-14 19:03:48 +0200

    all done!

---
+++ index.js
@@ -0,0 +1,8 @@
+var rand = Math.floor(Math.random()*6)+1;
+var randImage = "dice" + rand + ".png";
+var randFolder = "images/"+randImage;
+document.querySelector(".img1").setAttribute("src",randFolder);
+var rand = Math.floor(Math.random()*6)+1;
+var randImage = "dice" + rand + ".png";
+var randFolder = "images/"+randImage;
+document.querySelector(".img2").setAttribute("src",randFolder);
